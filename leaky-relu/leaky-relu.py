c4b059bc2c76bd47e43fc3aa2508ac4b3da247af
sairajuindukuri <133075265+sairajuindukuri@users.noreply.github.com>
2026-06-08 21:45:12 -0500

    Solve leaky-relu

--- leaky-relu/leaky-relu.py
+++ leaky-relu/leaky-relu.py
@@ -5,4 +5,5 @@ def leaky_relu(x, alpha=0.01):
     Vectorized Leaky ReLU implementation.
     """
     # Write code here
-    return np.maximum(np.array(x), alpha*np.array(x))
+    #return np.maximum(np.array(x), alpha*np.array(x))
+    return np.where(np.array(x, dtype =float)>=0, np.array(x), alpha*np.array(x))
